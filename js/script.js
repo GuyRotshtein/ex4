3aeda09d8879ec4a00301436e5ab40925263b580
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-05-09 00:42:30 +0300

    Some Fixes

--- js/script.js
+++ js/script.js
@@ -32,7 +32,7 @@ function validateForm() {
     if (!onePassword || !regPass.test(onePassword)) {
         document.getElementById(`invalidPassword`).style.display = `block`;
         window.alert(`Please rewrite the license plate to include at least one capital letter, small letter, number, and to be at lest 8 letters long.`);
-        if (errorFlag = 0){
+        if (errorFlag == 0){
             window.setTimeout(function() { document.formOne.pass.focus(); },0);
             errorFlag = 1;
         }
@@ -41,7 +41,7 @@ function validateForm() {
     if (!oneMail || !regMail.test(oneMail)){
         document.getElementById(`invalidMail`).style.display = `block`;
         window.alert(`Please write your e-mail address to include a '@' and a dot, and try again`);
-        if (errorFlag = 0){
+        if (errorFlag == 0){
             window.setTimeout(function() { document.formOne.mail.focus(); },0);
             errorFlag = 1;
         }
@@ -50,7 +50,7 @@ function validateForm() {
     if (!onePhone || onePhone.length <9 || onePhone.length > 10 || !regPhone.test(onePhone)){
         document.getElementById(`invalidPhone`).style.display = `block`;
         window.alert(`Please write the marshal's phone number to be composed only of numbers and 9-10 letters long.`);
-        if (errorFlag = 0){
+        if (errorFlag == 0){
             window.setTimeout(function() { document.formOne.phone.focus(); },0);
             errorFlag = 1;
         }
@@ -85,7 +85,7 @@ function validateForm() {
         }
     }
     // if there is an error, don't submit the form
-    if (errorFlag = 1){
+    if (errorFlag == 1){
         errorFlag = 0;
         return false
     }
@@ -98,16 +98,15 @@ function validateEditForm() {
     let checkColor = false;
 
     //resetting the errors so any fixed ones don't remain on screen
-    let errorFlag = 0;
+    let editFlag = 0;
     document.getElementById(`invalidSize`).style.display = `none`;
     document.getElementById(`invalidColor`).style.display = `none`;
-
     //checking that a size was selected
-    if(editSize.value === "default"){
+    if(editSize.value == "default"){
         document.getElementById(`invalidSize`).style.display = `block`;
         window.alert(`Please select a size from the dropdown list.`);
         window.setTimeout(function() { editSize.focus(); },0);
-        errorFlag = 1;
+        editFlag = 1;
     }
     //checking if a color is selected
     for (let i = 0; i < editColor.length; i++) {
@@ -119,14 +118,15 @@ function validateEditForm() {
     if (!checkColor){
         document.getElementById(`invalidColor`).style.display = `block`;
         window.alert(`Please select a color from the list.`);
-        if (errorFlag = 0){
+        if (editFlag == 0){
             window.setTimeout(function() { editColor[0].focus(); },0);
-            errorFlag = 1;
+            editFlag = 1;
         }
+        editFlag = 1;
     }
     // if there is an error, don't submit the form
-    if (errorFlag = 1){
-        errorFlag = 0;
+    if (editFlag == 1){
+        editFlag = 0;
         return false
     }
     return true;
